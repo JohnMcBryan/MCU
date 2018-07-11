a93f1d2bc403de8ab85fa01351464389ce23e7aa
John McBryan <johnmcbryan5@gmail.com>
2018-07-10 22:48:16 -0400

    1-tutorial-setup:
    - Added the ability to edit articles
    - Added the ability to add new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,27 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to @article
+  end
+  def new
+    @article = Article.new
+  end
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit!
+  end
+
 end
