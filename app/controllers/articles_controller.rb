a255a219da07f13ccd1c5764b78c84f78aa28dee
JohnMcBryan <johnmcbryan5@gmail.com>
2018-07-18 21:18:07 -0400

    Set up editing for article forms.  Also moved some of the views into a partial.  Added delete but its not working.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,11 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to @article
   end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 
   private
 
