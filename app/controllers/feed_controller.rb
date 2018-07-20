c7a846b812a81b7f45b1bd07e48d2e60a4a04bb8
JohnMcBryan <johnmcbryan5@gmail.com>
2018-07-20 19:19:42 -0400

    3-Twitter_integration:
    - Initial Twitter Try
    - SSL Auth Failure

---
+++ app/controllers/feed_controller.rb
@@ -0,0 +1,18 @@
+class FeedController < ApplicationController
+
+  require 'twitter'
+
+
+
+  def index
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = "VUEEw7a6mJFvPw2XEKFaixDmQ"
+      config.consumer_secret     = "Ou8J7DCWtpjXEByHU44nolPdCZsKGijYAv3kdSKiizOLzWUvXA"
+      config.access_token        = "744931399849803776-CCoXjur2NOJlK72qTbI3sFIlfOSS0Zf"
+      config.access_token_secret = "az9JlCtqX8mDeIWu3eyA2hEu0Xp0MMvsNSo2zszNQnqqS"
+    end
+
+    @tweets = client.user_timeline('rubyinside', count: 20)
+  end
+
+end
